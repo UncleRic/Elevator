75a0a251b9fe50063a407519d69acc5dc3d5244d
Frederick C. Lee <frederick.c.lee@gmail.com>
2016-02-26 18:43:41 -0800

    Elementary Animation of Carriage A.

--- Elevator/ViewController.swift
+++ Elevator/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class BuildingViewController: UIViewController {
+enum floor:CGFloat {
+    case top = 30.0
+    case fifth = 100.0
+    case forth = 170.0
+    case third = 240.0
+    case second = 310.0
+    case first = 380.0
+    case ground = 450.0
+}
 
+class BuildingViewController: UIViewController {
+    @IBOutlet weak var ricLabel: UILabel!
+    @IBOutlet weak var ricLabelConstraint: NSLayoutConstraint!
+    @IBOutlet weak var carriageA: UIView!
+    @IBOutlet weak var carriageB: UIView!
+    @IBOutlet weak var carriageC: UIView!
+    @IBOutlet weak var carriageD: UIView!
+    
     @IBOutlet weak var carriageABottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var carriageBBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var carriageCBottomConstraint: NSLayoutConstraint!
@@ -21,17 +37,26 @@ class BuildingViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // -----------------------------------------------------------------------------------------------------
     
-        @IBAction func exitAction(sender: UIBarButtonItem) {
-            exit(0)
+    @IBAction func animateAction(sender: UIBarButtonItem) {
+        let myDuration:NSTimeInterval = 3.0
+        UIView.animateWithDuration(myDuration) {
+            var myCenter = self.carriageA.center
+            myCenter.y = floor.ground.rawValue
+            self.carriageA.center = myCenter
+        }
     }
-
+    
+    @IBAction func exitAction(sender: UIBarButtonItem) {
+        exit(0)
+    }
+    
 }
 
